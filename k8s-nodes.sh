eeda96cf38f8b8941162bea4e146569af6dc381a
Vishesh Chikara <visheshchikara@gmail.com>
2025-11-21 17:11:54 +0530

    Update cri-dockerd installation method
    Download and install cri-dockerd directly from GitHub instead of using a script.

--- k8s-nodes.sh
+++ k8s-nodes.sh
@@ -9,13 +9,19 @@ sudo systemctl restart docker.service
 
 ## Install CRI-Docker
 
+VER="0.3.21"                               # or the version you want
+ARCH=$(dpkg --print-architecture)          # amd64 or arm64
 
-sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installCRIDockerd.sh -P /tmp
 
+wget "https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.${ARCH}.tgz"
+tar -xzvf "cri-dockerd-${VER}.${ARCH}.tgz"
+sudo mv cri-dockerd/cri-dockerd /usr/bin/
+sudo chmod 755 /usr/bin/cri-dockerd
 
-sudo chmod 755 /tmp/installCRIDockerd.sh
-sudo bash /tmp/installCRIDockerd.sh
+sudo systemctl daemon-reload
 sudo systemctl restart cri-docker.service
+sudo systemctl status cri-docker.service --no-pager -l
+
 
 
 ## Install kuubernetes
