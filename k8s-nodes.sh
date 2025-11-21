71b74eb6d9cb07f048ffc738a66fa2c174c5cc91
Vishesh Chikara <visheshchikara@gmail.com>
2025-11-21 17:29:15 +0530

    Refactor k8s-nodes.sh to simplify installation process
    Removed Docker service management and cri-dockerd installation steps.

--- k8s-nodes.sh
+++ k8s-nodes.sh
@@ -14,26 +14,6 @@ sudo chmod 755 /tmp/installCRIDockerd.sh
 sudo bash /tmp/installCRIDockerd.sh
 sudo systemctl restart cri-docker.service
 
-sudo systemctl enable docker
-sudo systemctl restart docker
-sudo systemctl restart cri-docker.service
-
-
-VER="0.3.21"                               # or the version you want
-ARCH=$(dpkg --print-architecture)          # amd64 or arm64
-
-
-cd /tmp
-sudo rm -rf cri-dockerd*
-
-wget "https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.${ARCH}.tgz"
-tar -xzvf "cri-dockerd-${VER}.${ARCH}.tgz"
-sudo mv cri-dockerd/cri-dockerd /usr/bin/
-sudo chmod 755 /usr/bin/cri-dockerd
-
-sudo systemctl daemon-reload
-sudo systemctl restart cri-docker.service
-sudo systemctl status cri-docker.service --no-pager -l
 
 
 
