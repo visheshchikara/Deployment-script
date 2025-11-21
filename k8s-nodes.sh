b41f65b5465cecbe99c7054783c6004f29e3ec18
Vishesh Chikara <visheshchikara@gmail.com>
2025-11-21 17:25:57 +0530

    Install CRI-Docker and configure services
    Download and install CRI-Docker, restart services, and clean up temporary files.

--- k8s-nodes.sh
+++ k8s-nodes.sh
@@ -9,9 +9,22 @@ sudo systemctl restart docker.service
 
 ## Install CRI-Docker
 
+sudo wget https://raw.githubusercontent.com/lerndevops/labs/master/scripts/installCRIDockerd.sh -P /tmp
+sudo chmod 755 /tmp/installCRIDockerd.sh
+sudo bash /tmp/installCRIDockerd.sh
+sudo systemctl restart cri-docker.service
+
+sudo systemctl enable docker
+sudo systemctl restart docker
+sudo systemctl restart cri-docker.service
+
+
 VER="0.3.21"                               # or the version you want
 ARCH=$(dpkg --print-architecture)          # amd64 or arm64
 
+
+cd /tmp
+sudo rm -rf cri-dockerd*
 
 wget "https://github.com/Mirantis/cri-dockerd/releases/download/v${VER}/cri-dockerd-${VER}.${ARCH}.tgz"
 tar -xzvf "cri-dockerd-${VER}.${ARCH}.tgz"
